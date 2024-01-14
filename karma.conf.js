d14cae3003ba01da154a707d240f9068f72ceb62
BijanProgrammer <bijaneisapour@gmail.com>
2024-01-14 18:49:44 +0330

    [UPDATE] domain and project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
             suppressAll: true, // removes the duplicated traces
         },
         coverageReporter: {
-            dir: require('path').join(__dirname, './coverage/code-star'),
+            dir: require('path').join(__dirname, './coverage/codestar'),
             subdir: '.',
             reporters: [{type: 'html'}, {type: 'text-summary'}],
         },
